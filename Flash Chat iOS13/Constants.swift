4a007a527d9a0cb6c5085a55d7618b5cca6af991
matthewogtong <ogtongm@gmail.com>
2022-01-06 13:55:25 -0500

    Add Constans file for strings and refactor code to reflect new struct

---
+++ Flash Chat iOS13/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  Flash Chat iOS13
+//
+//  Created by Matthew Ogtong on 1/6/22.
+//  Copyright © 2022 Angela Yu. All rights reserved.
+//
+
+struct Constants {
+    
+    static let registerSegue = "RegisterToChat"
+    static let loginSegue = "LoginToChat"
+    
+}
