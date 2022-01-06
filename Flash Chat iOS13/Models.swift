386a228d59fc7fc984be272de1569700b9a4b321
matthewogtong <ogtongm@gmail.com>
2022-01-06 14:33:55 -0500

    Create message struct model

---
+++ Flash Chat iOS13/Models.swift
@@ -0,0 +1,16 @@
+//
+//  Models.swift
+//  Flash Chat iOS13
+//
+//  Created by Matthew Ogtong on 1/6/22.
+//  Copyright © 2022 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Models {
+    
+    let sender: String
+    let message: String
+    
+}
