4bb3175b836c0ad72c50e213a24ddfa51caa0c43
matthewogtong <ogtongm@gmail.com>
2022-01-06 12:53:26 -0500

    Set up initial auth for registering users

--- Flash Chat iOS13/Controllers/RegisterViewController.swift
+++ Flash Chat iOS13/Controllers/RegisterViewController.swift
@@ -19,7 +19,11 @@ class RegisterViewController: UIViewController {
         if let email = emailTextfield.text, let password = passwordTextfield.text {
             
             Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
-                
+                if let e = error {
+                    print(e)
+                } else {
+                    self.performSegue(withIdentifier: "RegisterToChat", sender: self)
+                }
             }
             
         }
