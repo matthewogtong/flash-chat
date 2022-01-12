2ce905daaf8e97d6f38cc3b08d82b622ed39c9ce
matthewogtong <ogtongm@gmail.com>
2022-01-12 13:11:58 -0500

    Disable autoToolBar for IQKeyboardManager

--- Flash Chat iOS13/AppDelegate.swift
+++ Flash Chat iOS13/AppDelegate.swift
@@ -20,6 +20,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         FirebaseApp.configure()
         
         IQKeyboardManager.shared.enable = true
+        IQKeyboardManager.shared.enableAutoToolbar = false
         
         return true
     }
