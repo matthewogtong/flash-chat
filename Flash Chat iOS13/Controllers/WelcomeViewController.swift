52d9190612d68f37dc0c6ccaa47f795ebbbb23ed
matthewogtong <ogtongm@gmail.com>
2022-01-14 12:55:54 -0500

    Finishing touches for now

--- Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -11,7 +11,7 @@ import CLTypingLabel
 
 class WelcomeViewController: UIViewController {
 
-    @IBOutlet weak var titleLabel: CLTypingLabel!
+    @IBOutlet weak var titleLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
